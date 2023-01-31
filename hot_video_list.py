3b96515a4307079edf81f90197ce7e2cde0729ea
oodeng98 <oodeng98@naver.com>
2023-01-31 22:15:32 +0900

    코드 보완 및 기존 데이터 외에 추가 데이터 가져오는 코드도 추가

--- hot_video_list.py
+++ hot_video_list.py
@@ -23,34 +23,44 @@ with open('./video_category.json', 'r') as file:
 now = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d-%H-%M")
 filePath = f'./most_popular_videos/{now}'
 data = []
+if 'most_popular_videos' not in os.listdir():
+    os.mkdir('most_popular_videos')
+
 if now not in os.listdir('./most_popular_videos'):
     os.mkdir(filePath)
     os.mkdir(filePath + '/image')
 
-for categoryId in tqdm(category_dic['category_id']):
+for categoryId in tqdm(category_dic):
     count = 0
     try:
-        response = youtube.videos().list(part='snippet', chart='mostPopular', regionCode='kr',
+        response = youtube.videos().list(part='snippet, statistics, topicDetails', chart='mostPopular', regionCode='kr',
                                          videoCategoryId=categoryId, maxResults=50).execute()
     except googleapiclient.errors.HttpError:
         continue
-
+    # response = json.dumps(response['items'][0], indent="\t")
+    # print(response)
+    # exit()
     for item in response['items']:
         channelTitle = item['snippet']['channelTitle']
         channelId = item['snippet']['channelId']
         description = item['snippet']['description']
         title = item['snippet']['title']
         print(title)
-        publishedAt = item['snippet']['publishedAt']
+        publishedAt = item['snippet']['publishedAt']  # 이거 년월일시분으로 나눠야할듯
         try:
             tags = item['snippet']['tags']
         except KeyError:
             tags = ''
-        url = item['snippet']['thumbnails']['default']['url']
-        videoId = url.split('/')[-2]
-        # print(title)
+
+        videoId = item['id']
         imageFilePath = filePath + f'/image/{videoId}.jpg'
-        urllib.request.urlretrieve(url, imageFilePath)
+        urllib.request.urlretrieve(item['snippet']['thumbnails']['default']['url'], imageFilePath)
+
+        viewCount = item['statistics']['viewCount']
+        likeCount = item['statistics']['likeCount']
+        commentCount = item['statistics']['commentCount']
+
+        topicCategories = item['topicDetails']['topicCategories']
 
         data.append((channelTitle, channelId, description, title, publishedAt, tags, imageFilePath, videoId))
 df = pd.DataFrame(data, columns=['channelTitle', 'channelId', 'description', 'title', 'publishedAt', 'tags',
